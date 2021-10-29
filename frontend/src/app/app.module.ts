3893b806fcbf2b227b2e0de83554fff602a21d94
RavinduHashan <ravinduhashan78@gmail.com>
2021-10-29 17:07:43 +0530

    commit 23

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -81,6 +81,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HttpClientModule,
     NgbModule,
     FontAwesomeModule,
+    ReactiveFormsModule
 
   ],
   providers: [{
